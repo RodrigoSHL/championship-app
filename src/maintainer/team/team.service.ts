5041fb5aca04c8b4f0ffed980332154cc7d1150e
RodrigoSHL <shl.catalan@gmail.com>
2023-02-12 13:53:22 -0300

    FEAT: add services for delete, pagination and findOne

--- src/maintainer/team/team.service.ts
+++ src/maintainer/team/team.service.ts
@@ -3,13 +3,15 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { Repository } from 'typeorm';
 import { CreateTeamDto } from './dto/create-team.dto';
 import { UpdateTeamDto } from './dto/update-team.dto';
 import { Team } from './entities/team.entity';
-
+import { validate as IsUUID } from 'uuid';
 @Injectable()
 export class TeamService {
   private readonly logger = new Logger('TeamService');
@@ -29,12 +31,26 @@ export class TeamService {
     }
   }
 
-  async findAll() {
-    return await this.teamRepository.find();
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return this.teamRepository.find({
+      take: limit,
+      skip: offset,
+      // TODO: relations
+    });
   }
 
-  async findOne(id: string) {
-    return await this.teamRepository.find();
+  async findOne(term: string) {
+    let team: Team;
+    if (IsUUID(term)) {
+       team = await this.teamRepository.findOneBy({ id: term });
+    } else {
+       team = await this.teamRepository.findOneBy({ shortName: term });
+    }
+
+    if (!team) throw new NotFoundException(`Team with id ${term} not found`);
+    return team;
   }
 
   update(id: number, updateTeamDto: UpdateTeamDto) {
@@ -42,19 +58,17 @@ export class TeamService {
   }
 
   async remove(id: string) {
-    const team = await this.findOne( id );
-    await this.teamRepository.remove( team );
-    
+    const team = await this.findOne(id);
+    await this.teamRepository.remove(team);
   }
 
-  private handleDBExceptions( error: any ) {
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
 
-    if ( error.code === '23505' )
-      throw new BadRequestException(error.detail);
-    
-    this.logger.error(error)
+    this.logger.error(error);
     // console.log(error)
-    throw new InternalServerErrorException('Unexpected error, check server logs');
-
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs',
+    );
   }
 }
